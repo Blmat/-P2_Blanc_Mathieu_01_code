3598eca6988a4dd511f20204d8d54c6b39fdf2e9
Mathieu Blanc <94637553+Blmat@users.noreply.github.com>
2022-02-02 01:09:43 +0100

    1.8 final

--- src/com/company/Mathieu/CountSymptoms.java
+++ src/com/company/Mathieu/CountSymptoms.java
@@ -3,6 +3,7 @@ package com.company.Mathieu;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
 /**
  * Create a map and read the list
  * <p>
@@ -12,13 +13,12 @@ import java.util.TreeMap;
  * <p>
  * Else: add symptoms and an occurrence(value) of 1 in the map
  */
-public class CountSymptoms implements ICountSymptoms{
+public class CountSymptoms implements ICountSymptoms {
     @Override
     public Map<String, Integer> countSymptoms(List<String> result) {
-
         Map<String, Integer> tSymptomMap = new TreeMap<>();
         result.forEach(s -> {
-            int value=tSymptomMap.getOrDefault(s ,0)+1;
+            int value = tSymptomMap.getOrDefault(s, 0) + 1;
             tSymptomMap.put(s, value);
         });
         System.out.println("Total symptom = " + tSymptomMap.size());
--- src/com/company/Mathieu/ISymptomFromDateFile.java
+++ src/com/company/Mathieu/ISymptomFromDateFile.java
@@ -1,18 +1,19 @@
 package com.company.Mathieu;
 
 import java.util.List;
+
 /**
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
  * that may contain many duplications
- *
+ * <p>
  * The implementation does not need to order the list
- *
  */
 public interface ISymptomFromDateFile {
     /**
      * If no data is available, return an empty List
+     *
      * @return a listing of all Symptoms obtained from a data source, duplicates are possible
      */
-    List<String>getSymptoms ();
+    List<String> getSymptoms();
 }
